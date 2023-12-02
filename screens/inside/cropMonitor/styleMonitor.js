67d1c6a27d92995ffd95f9e9d6dfa21feab6ba74
chuong200115 <84147271+chuong200115@users.noreply.github.com>
2023-12-02 13:43:40 +0700

    Update styleMonitor.js

--- screens/inside/cropMonitor/styleMonitor.js
+++ screens/inside/cropMonitor/styleMonitor.js
@@ -52,6 +52,77 @@ const styles = StyleSheet.create({
   },
   nhietDo: {
     fontWeight: 'bold'
+  },
+  soilArea: {
+    flexDirection: 'row',
+    flex: 1,
+    marginVertical: 20,
+  },
+  nitrogenArea: {
+    height: 110,
+    width: 110,
+    borderColor: '#fff',
+    borderWidth: 7,
+    backgroundColor: '#A8FAF4',
+    marginHorizontal: 10,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  photphorusArea: {
+    height: 110,
+    width: 110,
+    borderColor: '#fff',
+    borderWidth: 7,
+    backgroundColor: '#a5d2f5',
+    marginHorizontal: 10,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  kaliArea: {
+    height: 110,
+    width: 110,
+    borderColor: '#fff',
+    borderWidth: 7,
+    backgroundColor: '#a2aaf5',
+    marginHorizontal: 10,
+    borderRadius: 100,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  soilText: {
+    marginVertical: 5,
+    fontWeight: 'bold'
+  },
+  electricArea: {
+    flexDirection: 'row',
+    flex: 1,
+    marginBottom: 20,
+  },
+  elecConducArea: {
+    height: 110,
+    width: 170,
+    borderColor: '#fff',
+    borderWidth: 5,
+    backgroundColor: '#bccdf8',
+    borderRadius: 20,
+    marginLeft: 20,
+    marginRight: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  potenHydroArea: {
+    height: 110,
+    width: 170,
+    borderColor: '#fff',
+    borderWidth: 5,
+    backgroundColor: '#a1e3d9',
+    borderRadius: 20,
+    marginLeft: 10,
+    marginRight: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   }
 })
 
